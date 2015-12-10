8103ad9e3f4af7e804e15e293ac245fbc5ee8061
sazmine1 <sazmine@stuy.edu>
2015-12-10 07:03:19 -0500

    Update SuperArray.java

--- SuperArray.java
+++ SuperArray.java
@@ -23,7 +23,7 @@ public class SuperArray {
  
     //~~~~~INSTANCE VARS~~~~~
     //underlying container, or "core" of this data structure:
-    private int[] _data;
+    private Comparable[] _data;
 
     //position of last meaningful value
     private int _lastPos;
@@ -33,10 +33,10 @@ public class SuperArray {
 
 		
     //~~~~~METHODS~~~~~
-    //default constructor Ã¢â‚¬â€œ initializes 10-item array
+    //default constructor initializes 10-item array
     public SuperArray() 
     { 
-	_data = new int[10];
+	_data = new Comparable[10];
 	_lastPos = -1; //flag to indicate no lastpos yet
 	_size = 0;	
     }
@@ -61,7 +61,7 @@ public class SuperArray {
     //double capacity of this SuperArray
     private void expand() 
     { 
-	int[] temp = new int[ _data.length * 2 ];
+	Comparable[] temp = new Comparable[ _data.length * 2 ];
 	for( int i = 0; i < _data.length; i++ )
 	    temp[i] = _data[i];
 	_data = temp;
@@ -69,7 +69,7 @@ public class SuperArray {
 
 		
     //accessor -- return value at specified index
-    public int get( int index ) {
+    public Comparable get( int index ) {
         return _data[index]; 
         
     }
@@ -77,9 +77,9 @@ public class SuperArray {
 		
     //mutator -- set value at index to newVal, 
     //           return old value at index
-    public int set( int index, int newVal ) 
+    public Comparable set( int index, Comparable newVal ) 
     { 
- 	int temp = _data[index];
+ 	Comparable temp = _data[index];
 	_data[index] = newVal;
 	return temp;
     }
@@ -87,8 +87,8 @@ public class SuperArray {
 
     // ~~~~~~~~~~~~~~ PHASE II ~~~~~~~~~~~~~~
     //adds an item after the last item
-    public void add( int newVal ) { 
-        int b [] = new int [_data.length + 1];
+    public void add( Comparable newVal ) { 
+        Comparable b [] = new Comparable [_data.length + 1];
         for (int x = 0; x < _data.length; x++) {
             b[x] = _data[x];
         }
@@ -101,20 +101,18 @@ public class SuperArray {
 
     //inserts an item at index
     //shifts existing elements to the right
-    public void add( int index, int newVal ) {
-    	if (_data.length>index&&index>-1){
-	    int[] b=new int[_data.length+1];
-	    for(int x=0;x<index;x++){
-		b[x]=_data[x];
-	    }
-	    b[index]=newVal;
-	    for(int x=index+1;x<_data.length;x++){
-		b[x]=_data[x-1];
-	    }
-	    _data=b;
-	    _lastPos+=1;
-	    _size+=1;
-    	}
+    public void add( int index, Comparable newVal ) {
+	if (_size == _data.length)
+	    expand();
+	else if (index > _size){
+	    _size ++;
+	    _lastPos ++;
+	}
+	else {
+	    Comparable temp = _data[index];
+	    _data[index] = newVal;
+	    add(index+1, temp);
+	}
     }
 
 
@@ -132,69 +130,175 @@ public class SuperArray {
 
     //return number of meaningful items in _data
     public int size() { 
-        return (_lastPos + 1);
+        return _size;
+    }
+
+    // ~~~~~~~~~~~~~~ PHASE III ~~~~~~~~~~~~~~
+    //finds index of first appearance of an element
+    public int linSearch(Comparable a) {
+    	
+	//inital counter, so if target is not in array, index will be -1
+	int ans = -1;
+	for (int x = 0; x < _size; x++) {
+	    //checks to see if any index of the array is the same as the target
+	    if(_data[x] == a) {
+		ans = x;
+		return ans;
+	    }
+	    //stops loop after finding the first matching target
+	}
+	return ans;
+    }
+
+    public boolean isSorted() {
+	boolean retVal = false;
+	if (_size == 1) {
+	    retVal = true;
+	}
+	else{
+	    for (int x = 0; x < _size; x++) {
+		if ((_data[x].compareTo(_data[x+1])) == 1) {
+		    retVal = false;
+		}
+		else {
+		    retVal = true;
+		}
+	    }
+	}
+	return retVal;
     }
 
 
     //main method for testing
     public static void main( String[] args ) 
     {
-	SuperArray curtis = new SuperArray();
-	System.out.println("Printing empty SuperArray curtis...");
-	System.out.println(curtis);
-
-	for( int i = 0; i < curtis._data.length; i++ ) {
-	    curtis.set(i,i*2);
-	    curtis._size++; //necessary bc no add() method yet
-	}
-
-	System.out.println("Printing populated SuperArray curtis...");
-	System.out.println(curtis);
-
-	System.out.println("testing get()...");
-	for( int i = 0; i < curtis._size; i++ ) {
-	    System.out.print( "item at index" + i + ":\t" );
-	    System.out.println( curtis.get(i) );
-	}
-
-	System.out.println("Expanded SuperArray curtis:");
-	curtis.expand();
-	System.out.println(curtis);
-
-
-	SuperArray mayfield = new SuperArray();
-	System.out.println("Printing empty SuperArray mayfield...");
-	System.out.println(mayfield);
-
-	mayfield.add(5);
-	mayfield.add(4);
-	mayfield.add(3);
-	mayfield.add(2);
-	mayfield.add(1);
-
-	System.out.println("Printing populated SuperArray mayfield...");
-	System.out.println(mayfield);
-
-	mayfield.remove(3);
-	System.out.println("Printing SuperArray mayfield post-remove...");
-	System.out.println(mayfield);
-	mayfield.remove(3);
-	System.out.println("Printing SuperArray mayfield post-remove...");
-	System.out.println(mayfield);
-
-	mayfield.add(3,99);
-	System.out.println("Printing SuperArray mayfield post-insert...");
-	System.out.println(mayfield);
-	mayfield.add(2,88);
-	System.out.println("Printing SuperArray mayfield post-insert...");
-	System.out.println(mayfield);
-	mayfield.add(1,77);
-	System.out.println("Printing SuperArray mayfield post-insert...");
-	System.out.println(mayfield);
 	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+	  SuperArray curtis = new SuperArray();
+	  System.out.println("Printing empty SuperArray curtis...");
+	  System.out.println(curtis);
+
+	  for( int i = 0; i < curtis._data.length; i++ ) {
+	  curtis.set(i,i*2);
+	  curtis._size++; //necessary bc no add() method yet
+	  }
+
+	  System.out.println("Printing populated SuperArray curtis...");
+	  System.out.println(curtis);
+
+	  System.out.println("testing get()...");
+	  for( int i = 0; i < curtis._size; i++ ) {
+	  System.out.print( "item at index" + i + ":\t" );
+	  System.out.println( curtis.get(i) );
+	  }
+
+	  System.out.println("Expanded SuperArray curtis:");
+	  curtis.expand();
+	  System.out.println(curtis);
+
+
+	  SuperArray mayfield = new SuperArray();
+	  System.out.println("Printing empty SuperArray mayfield...");
+	  System.out.println(mayfield);
+
+	  mayfield.add(5);
+	  mayfield.add(4);
+	  mayfield.add(3);
+	  mayfield.add(2);
+	  mayfield.add(1);
+
+	  System.out.println("Printing populated SuperArray mayfield...");
+	  System.out.println(mayfield);
+
+	  mayfield.remove(3);
+	  System.out.println("Printing SuperArray mayfield post-remove...");
+	  System.out.println(mayfield);
+	  mayfield.remove(3);
+	  System.out.println("Printing SuperArray mayfield post-remove...");
+	  System.out.println(mayfield);
+
+	  mayfield.add(3,99);
+	  System.out.println("Printing SuperArray mayfield post-insert...");
+	  System.out.println(mayfield);
+	  mayfield.add(2,88);
+	  System.out.println("Printing SuperArray mayfield post-insert...");
+	  System.out.println(mayfield);
+	  mayfield.add(1,77);
+	  System.out.println("Printing SuperArray mayfield post-insert...");
+	  System.out.println(mayfield);
+      
 	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 	//*****INSERT ANY ADDITIONAL TEST CALLS HERE*****
+	SuperArray lois = new SuperArray();
+	System.out.println("Printing empty SuperArray lois...");
+	System.out.println(lois);
 
+	lois.add(new Hexadecimal(55));
+	lois.add(new Hexadecimal("10"));
+	lois.add(new Hexadecimal(12));
+	lois.add(new Hexadecimal("7"));
+	lois.add(new Hexadecimal(1));
+
+       	System.out.println("Printing populated SuperArray lois...");
+	System.out.println(lois);
+
+       	lois.remove(3);
+	System.out.println("Printing SuperArray lois post-remove...");
+	System.out.println(lois);
+	lois.remove(3);
+	System.out.println("Printing SuperArray lois post-remove...");
+	System.out.println(lois);
+
+	lois.add(3, new Hexadecimal("99"));
+	System.out.println("Printing SuperArray lois post-insert...");
+	System.out.println(lois);
+	lois.add(2, new Hexadecimal(88));
+	System.out.println("Printing SuperArray lois post-insert...");
+	System.out.println(lois);
+	lois.add(1, new Hexadecimal("77"));
+	System.out.println("Printing SuperArray lois post-insert...");
+	System.out.println(lois);
+
+	Hexadecimal lane = new Hexadecimal("77");
+       	System.out.println(lois.linSearch(new Rational(8,8)));
+       	System.out.println(lois.linSearch(lane));
+
+	System.out.println(lois.isSorted());
+	/*	
+		SuperArray clark = new SuperArray();
+		System.out.println("Printing empty SuperArray clark...");
+		System.out.println(clark);
+
+		clark.add(new Binary(55));
+		clark.add(new Hexadecimal("10"));
+		clark.add(new Rational(1,2));
+		clark.add(new Binary("010"));
+		clark.add(new Hexadecimal(1));
+
+		System.out.println("Printing populated SuperArray clark...");
+		System.out.println(clark);
+
+		clark.remove(3);
+		System.out.println("Printing SuperArray clark post-remove...");
+		System.out.println(clark);
+		clark.remove(3);
+		System.out.println("Printing SuperArray clark post-remove...");
+		System.out.println(clark);
+
+		clark.add(3, new Hexadecimal("99"));
+		System.out.println("Printing SuperArray clark post-insert...");
+		System.out.println(clark);
+		clark.add(2, new Rational(8,8));
+		System.out.println("Printing SuperArray clark post-insert...");
+		System.out.println(clark);
+		clark.add(1, new Binary(77));
+		System.out.println("Printing SuperArray clark post-insert...");
+		System.out.println(clark);
+
+		System.out.println(clark.linSearch(new Rational(8,8)));
+		System.out.println(clark.linSearch(new Hexadecimal("13")));
+
+		System.out.println(clark.isSorted());
+	*/
     }//end main
 		
 }//end class
